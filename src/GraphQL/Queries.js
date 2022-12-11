1a39640d5675d1b3a4e5cb1a52fc2de1f05ccc00
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-12-11 19:59:39 +0100

    display products table

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -1,5 +1,18 @@
 import {gql} from'@apollo/client'
 
 export const GET_PRODUCTS = gql`
+query{
+    GetAllProducts{
+      id,
+      product_name,
+      overall_rating,
+      product_description,
+      main_category,
+      sub_category,
+      price,
+      link
+      
+    }
+  }
 
 `
