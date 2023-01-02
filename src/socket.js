610957d6fcba5896e4eb4c8acc0f4f54cb65f73e
aleksanderminchev <83026219+aleksanderminchev@users.noreply.github.com>
2023-01-02 23:55:20 +0200

    final socket change

--- src/socket.js
+++ src/socket.js
@@ -11,34 +11,26 @@ export const loginSocket=(name)=> socket.emit('login',{userId:name})
 export const disconnectSocket=(name)=> socket.emit('disconnect',{userId:name})
 socket.on("online", data => {
     console.log(`${data} is online` )
-    let container = document.createElement("div")
-    container.innerText = data
-    container.className = data
     let div = document.getElementById("div1")
-    div.appendChild(container)
+    div.innerText = `${data} is online`
+    div.className = data
 })
 
 socket.on("offline", data => {
-    let container = document.createElement("div")
-    container.innerText =` ${data} is offline`
-    container.className =  data
     let div = document.getElementById("div1")
-    div.appendChild(container)
+    div.innerText =` ${data} is offline`
+    div.className =  data
 })
 // socket.on("disconnect", () => {
 //     socket.emit("logout", parsedUser)
 //     }) 
 socket.on("user-not-found", (data) => {
-    let container = document.createElement("div")
-    container.innerText = `User is not found${data}`
-    container.className = data
     let div = document.getElementById("div1")
-    div.appendChild(container)
+    div.innerText = `User is not found${data}`
+    div.className = data
     }) 
 socket.on("invitation-pending", (email)=>{
     let div = document.getElementById("div1")
-    let container = document.createElement("div")
-    container.innerText = ` Waiting for approve from ${email}`
-    container.className =` Waiting for approve from ${email}`
-    div.appendChild(container)
+    div.innerText = ` Waiting for approve from ${email}`
+    div.className =email
     })
